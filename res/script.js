2db5b45f84adcdf6de61710c4831c9b49e19cfcd
Nighy Riy <34189002+Nighy01@users.noreply.github.com>
2023-10-22 10:56:12 +0800

    a Legacy is back...
    Resurrection Project: RedFance

--- res/script.js
+++ res/script.js
@@ -1,8 +1,9 @@
-// 2023/02/27
+// 2023/10/22
 // edited from source: https://codepen.io/DeveloperJone/pen/MWjvvJp
 
 const DLMSwitchBtn = document.querySelector("#navbar_selection_1 > button");
 const bodyDLM = document.querySelector("body");
+const OpenGithubLink = document.querySelector(".OpenGithubLink");
 
 const DLM = () => {
   bodyDLM.classList.toggle("dark");
@@ -25,3 +26,7 @@ let DLMSwitch = localStorage.getItem("dark");
 if (DLMSwitch === "on") {
   DLM();
 }
+
+OpenGithubLink.addEventListener("click", () => {
+  window.open('https://github.com/Nighy01/Nighy01.github.io', '_blank');
+});
